8c4acd9f99a8e960cfbe73090ad7ec5c20abd50c
KakarlaRoopesh <84217769+KakarlaRoopesh@users.noreply.github.com>
2021-06-20 15:55:36 +0530

    sm

---
+++ urls.py
@@ -0,0 +1,23 @@
+
+
+from django.conf.urls import url
+from django.contrib import admin
+from django.urls import path,include
+
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns,static
+
+ 
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('accounts/',include('app_login.urls')),
+    path('',include('post.urls')),
+    
+]
+
+
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
